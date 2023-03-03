3e20dced7e3c57407a4a770f6d8c457c41dd1f32
John Lockwood <jlockwood@verticalrelevance.com>
2023-03-03 16:48:59 -0500

    Working example in Axum

---
+++ post_it.sh
@@ -0,0 +1,3 @@
+curl -X POST http://localhost:3000/user \
+   -H 'Content-Type: application/json' \
+   -d '{"email":"everyman@ubiquitous.com","password":"I_am_all_users"}'
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,47 @@
+// Needed for debug for CreateUser -- it's unclear why since this is used.
+#![allow(dead_code)]
+
 use serde::{Serialize, Deserialize};
-use serde_json;
+use axum::{Router, extract::Json, extract::Path};
+use axum::routing::{get, post};
+use std::net::SocketAddr;
 
-#[derive(Serialize, Deserialize, Debug)]
-enum ProgrammerType {
-    Brilliant,
-    Plodding,
-    Clueless
+// Input to post, user must supply password
+#[derive(Deserialize, Debug)]
+struct CreateUser {
+    email: String,
+    password: String,
 }
 
-
-#[derive(Serialize, Deserialize, Debug)]
-struct Person {
-    name: String,
-    height_inches:  u8,
-    books: Vec<String>,
-    programmer_type: ProgrammerType
+// Input to get
+#[derive(Serialize)]
+struct User {
+    email: String,
+    id: u32
 }
 
+#[tokio::main]
+async fn main() {
+    // build our application with a get and post route
+    let app = Router::new()
+        .route("/user", post(create_user))
+        .route("/user/:user_id", get(get_user));
 
-fn main() {
-    let person = Person{
-        name: "John".to_string(), 
-        height_inches: 73, 
-        books: vec!["At the Existentialist Cafe".to_string()], 
-        programmer_type: ProgrammerType::Plodding
-    };
+    // run it
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("listening on {}", addr);
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
 
-    // Convert the Person to a JSON string.
-    let serialized = serde_json::to_string(&person).unwrap();
+async fn get_user(Path(user_id) : Path<u32>) -> Json<User>  {
+    // Actually look up user from id, etc.
+    Json(User{email: "everyman@ubiquitous.com".to_string(), id: user_id})
+}
 
-    println!("serialized = {}", serialized);
-
-    // Convert the JSON string back to a Person.
-    let deserialized: Person = serde_json::from_str(&serialized).unwrap();
-
-    println!("deserialized = {:?}", deserialized);
+async fn create_user(Json(payload): Json<CreateUser>) {
+    // Check for existing, save to database, etc.
+    println!("Inside create_user:  {:?}", payload);
 }
